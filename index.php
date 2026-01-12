22c8f80205281d78ca65463c8cf80acaece617f9
Vadivukkarasi <vadivuvarun2612@gmail.com>
2026-01-12 17:32:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My EB PHP App</title>
+    <style>
+        body {
+            font-family: Arial;
+            background: #f4f6f8;
+            text-align: center;
+            padding-top: 50px;
+        }
+        h1 {
+            color: #ff9900;
+        }
+    </style>
+</head>
+<body>
+    <h1>Elastic Beanstalk Deployment Successful 🚀</h1>
+    <p>PHP Application via CodePipeline</p>
+</body>
+</html>
